85d4f9595884f9803aad0aea48807d759df44e21
soy-braised-pork <2428784410@qq.com>
2021-06-10 15:48:36 +0800

    update

---
+++ src/main/java/com/zlx/demo/controller/HttpServerHandler.java
@@ -0,0 +1,21 @@
+package com.zlx.demo.controller;
+
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelHandlerContext;
+
+public class HttpServerHandler implements ChannelHandler {
+    @Override
+    public void handlerAdded(ChannelHandlerContext channelHandlerContext) throws Exception {
+
+    }
+
+    @Override
+    public void handlerRemoved(ChannelHandlerContext channelHandlerContext) throws Exception {
+
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext channelHandlerContext, Throwable throwable) throws Exception {
+
+    }
+}
